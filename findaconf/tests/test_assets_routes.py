3755ebf381da94da7939d93deb91e6bd9a8f8a8e
Eduardo Cuducos <cuducos@gmail.com>
2014-12-10 11:27:03 +0000

    Simplify assets URL

--- findaconf/tests/test_assets_routes.py
+++ findaconf/tests/test_assets_routes.py
@@ -18,22 +18,22 @@ class TestAssetsRoutes(unittest.TestCase):
     # test assets from assets.yaml
 
     def test_libs_js(self):
-        resp = self.app.get('/assets/js/libs.min.js')
+        resp = self.app.get('/assets/libs.min.js')
         assert resp.status_code == 200
         assert resp.mimetype == 'application/javascript'
 
     def test_init_js(self):
-        resp = self.app.get('/assets/js/init.min.js')
+        resp = self.app.get('/assets/init.min.js')
         assert resp.status_code == 200
         assert resp.mimetype == 'application/javascript'
 
     def test_libs_css(self):
-        resp = self.app.get('/assets/css/libs.min.css')
+        resp = self.app.get('/assets/libs.min.css')
         assert resp.status_code == 200
         assert resp.mimetype == 'text/css'
 
     def test_style_css(self):
-        resp = self.app.get('/assets/css/style.min.css')
+        resp = self.app.get('/assets/style.min.css')
         assert resp.status_code == 200
         assert resp.mimetype == 'text/css'
 
