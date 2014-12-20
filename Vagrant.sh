655c804c206c040034544fc3ec07228c5358f887
Eduardo Cuducos <cuducos@gmail.com>
2014-12-20 21:10:44 +0000

    Add psql commands to create user/db

--- Vagrant.sh
+++ Vagrant.sh
@@ -20,7 +20,9 @@ npm install -g bower
 # PostgreSQL
 apt-get install -y postgresql postgresql-contrib libpq-dev
 apt-get install -y python-psycopg2
-sudo -u postgres createuser --superuser vagrant
+sudo -u postgres psql -c "CREATE ROLE vagrant WITH PASSWORD 'vagrant' LOGIN SUPERUSER"
+sudo -u postgres createdb -O vagrant findaconf
+sudo -u postgres createdb -O vagrant findaconf_test
 
 # install project dependencies
 pip install -r /vagrant/requirements.txt
