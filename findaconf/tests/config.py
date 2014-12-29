3007699011fcca9dfb9aaae4c14eb51b2ee7c3f7
Eduardo Cuducos <cuducos@gmail.com>
2014-12-29 13:00:39 +0000

    Add faker to create test data

--- findaconf/tests/config.py
+++ findaconf/tests/config.py
@@ -1,9 +1,11 @@
 # coding: utf-8
 
 from decouple import config
+from findaconf.tests.fake_data import fake_conference, seed
 
 
 def set_app(app, db=False):
+    unset_app(db)
     app.config['TESTING'] = True
     app.config['WTF_CSRF_ENABLED'] = False
     if db:
@@ -16,6 +18,9 @@ def set_app(app, db=False):
     test_app = app.test_client()
     if db:
         db.create_all()
+        seed(app, db)
+        [db.session.add(fake_conference(db)) for i in range(1, 43)]
+        db.session.commit()
     return test_app
 
 
