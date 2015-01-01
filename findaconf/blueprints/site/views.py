8dc8514a7c8e50ba40c20e8d10809c3b1e906860
Gabriel Vicente <gabrielusvicente@gmail.com>
2015-01-01 13:03:12 -0200

    Create login authentication

--- findaconf/blueprints/site/views.py
+++ findaconf/blueprints/site/views.py
@@ -1,10 +1,15 @@
 # coding: utf-8
 
 import sys
-from findaconf import app
-from findaconf.models import Conference, Continent, Country, Year
-from flask import Blueprint, render_template, request
+
+from authomatic import Authomatic
+from authomatic.adapters import WerkzeugAdapter
+from findaconf import app, db, lm
+from findaconf.models import Conference, Continent, Country, User, Year
+from flask import Blueprint, flash, g, redirect, render_template, request, make_response, url_for
+from flask.ext.login import current_user, login_user, logout_user
 from htmlmin.minify import html_minify
+
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -36,7 +41,57 @@ def results():
 @site_blueprint.route('/login', methods=['GET'])
 def login_options():
     return html_minify(render_template('login.slim',
-                                       providers=app.config['PROVIDERS']))
+                                       providers=app.config['OAUTH_CREDENDIALS'].keys()))
+
+
+@site_blueprint.route('/login/<provider_name>', methods=['GET', 'POST'])
+def login(provider_name):
+
+    # create authomatic and response objects
+    authomatic = Authomatic(app.config['OAUTH_CREDENDIALS'], 
+                            app.config['SECRET_KEY'], 
+                            report_errors=True)
+    response = make_response()
+    
+    # try login
+    result = authomatic.login(WerkzeugAdapter(request, response), provider_name)
+    if result:
+        
+        # if success
+        if result.user:
+            result.user.update()
+            
+            # manage user data in db
+            user = User.query.filter_by(email=result.user.email).first()
+            if not user:
+                new_user = User(email=result.user.email, name=result.user.name)
+                db.session.add(new_user)
+                db.session.commit()
+                user = User.query.filter_by(email=result.user.email).first()
+            
+            # save user info
+            login_user(user)
+            flash({'type': 'success', 'text': 'Welcome, {}'.format(result.user.name)})
+            return redirect(url_for('site.index'))
+    
+    return response
+
+
+@site_blueprint.route('/logout')
+def logout():
+    logout_user()
+    flash({'type': 'success', 'text': 'You\'ve been logged out.'})
+    return redirect(url_for('site.index'))
+
+
+@site_blueprint.before_request
+def before_request():
+    g.user = current_user
+
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 @site_blueprint.context_processor
