76abc1d6043a509418027c618d16c5a38502f2f2
Eduardo Cuducos <cuducos@gmail.com>
2015-01-01 21:00:12 +0000

    Add tests for login oauth links

--- findaconf/tests/test_site_routes.py
+++ findaconf/tests/test_site_routes.py
@@ -27,14 +27,31 @@ class TestSiteRoutes(TestCase):
                                            'location': 'University of Essex'})
         assert resp.status_code == 200
         assert resp.mimetype == 'text/html'
-    
+
     def test_login(self):
-        
+
         # test if login page exists
         resp = self.app.get('/login')
         assert resp.status_code == 200
         assert resp.mimetype == 'text/html'
-        
+
+        # test if are links to oauth/oauth2 providers
+        providers = app.config['OAUTH_CREDENTIALS'].keys()
+        for provider in providers:
+            assert 'href="/login/{}'.format(provider) in resp.data
+
         # test if is there a link to login in the home page
         resp = self.app.get('/')
         assert 'href="/login"' in resp.data
+
+    def test_login_providers(self):
+
+        # test if links to the ouauth/oauth2 providers
+        providers = app.config['OAUTH_CREDENTIALS'].keys()
+        for provider in providers:
+            resp = self.app.get('/login/{}'.format(provider))
+            assert resp.status_code == 200
+
+        # test if unauthorized provider returns 404
+        resp = self.app.get('/login/anything_else')
+        assert resp.status_code == 404
