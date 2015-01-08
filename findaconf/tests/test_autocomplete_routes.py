555637aa86bef0b3cf5d3fe67b0341bcee5e271a
Eduardo Cuducos <cuducos@gmail.com>
2015-01-08 17:07:36 +0000

    Add tests for 404 on invalid routes

--- findaconf/tests/test_autocomplete_routes.py
+++ findaconf/tests/test_autocomplete_routes.py
@@ -26,3 +26,16 @@ class TestAutoCompleteRoutes(TestCase):
         resp = self.app.get(url)
         assert resp.status_code == 200
         assert resp.mimetype == 'application/json'
+
+    def test_google_places_blank(self):
+        resp = self.app.get('/autocomplete/places?query=')
+        assert resp.status_code == 404
+        print resp.data
+
+    def test_google_places_wrong_proxy(self):
+        original_proxy = app.config['GOOGLE_PLACES_PROXY']
+        app.config['GOOGLE_PLACES_PROXY'] = 'http://python.org/ruby'
+        url = '/autocomplete/places?query=University'
+        resp = self.app.get(url)
+        assert resp.status_code == 404
+        app.config['GOOGLE_PLACES_PROXY'] = original_proxy
