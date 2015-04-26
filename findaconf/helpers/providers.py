c6ab5e6221743268b347b7f1761ce27871cd3f29
Eduardo Cuducos <cuducos@gmail.com>
2015-04-26 14:15:33 +0100

    More pythonic version of OAuthProvider()

--- findaconf/helpers/providers.py
+++ findaconf/helpers/providers.py
@@ -5,63 +5,62 @@ from findaconf import app
 
 class OAuthProvider(object):
 
+    """
+    Loads oauth/oauth2 credentials from app.config['OAUTH_CREDENTIALS'] and
+    sets the following attributes:
+
+    * original: (dictionary) the same credentials as loaded
+
+    * credentials: (list generator) tuples (slug, credentials) for a filtered
+    version of the loaded credentials keeping only `valid` oauth/oauth2
+    providers, i.e. the ones  with client key and secret properly set
+
+    * ordered: (list generator) OAuthProvider object for each valid
+    provider (sorted alphabetically)
+    """
+
     def __init__(self):
+        self.original = app.config['OAUTH_CREDENTIALS']
+
+    @property
+    def credentials(self):
         """
-        Loads oauth/oauth2 credentials from app.config['OAUTH_CREDENTIALS'] and
-        sets the following instance attributes:
-
-        * original_credentials: (dictionary) the same credentials as loaded
-
-        * credentials: (dictionary) filtered version of the loaded credentials
-        keeping only `valid` oauth/oauth2 providers, i.e. the ones  with client
-        key and secret properly set
-
-        * names: (list) humanized names for valid oauth/oauth2 providers (e.g.
-        Google Plus instead of google-plus)
-
-        * slugs: (list) slugs for valid oauth/oauth2 providers (e.g.
-        google-plus instead of Google Plus)
-
-        * providers: (dictionary) valid oauth/oauth2 providers having the slug
-        as key and the humanized name as value
+        Test each oauth/oauth2 provider to check if it has client and secret
+        keys set, and deleteing the ones with no settings. Returns a list
+        generator with tuples (provider name, provider settings).
         """
+        for provider in self.original.iterkeys():
+            credentials = self.original.get(provider)
+            if credentials:
+                key = credentials.get('consumer_key')
+                secret = credentials.get('consumer_secret')
+                if key and secret:
+                    provider = ProviderUI(name=provider)
+                    yield provider.slug, self.original[provider.name]
 
-        # load credentials & check if consumer key an secret are set
-        self.original_credentials = app.config['OAUTH_CREDENTIALS']
-        self.credentials = dict()
-        for provider in self.original_credentials.keys():
-            if self.__valid_provider(provider):
-                credential = self.original_credentials[provider]
-                self.credentials[provider] = credential
+    @property
+    def ordered(self):
+        """ List generator with OAuthProvider objects ordered by name  """
+        credentials = dict(self.credentials)
+        names = sorted([name for name in credentials.iterkeys()])
+        for name in names:
+            yield ProviderUI(name=name) 
+            
+    def name(self, slug):
+        """Returns a provider name given its slug"""
+        for provider in self.ordered:
+            if provider.slug == slug:
+                return provider.name
+        return None
 
-        # set support vars
-        self.names = sorted([p for p in self.credentials])
-        self.slugs = [p.lower().replace(' ', '-') for p in self.names]
-        self.providers = dict(zip(self.slugs, self.names))
+        
+class ProviderUI(object):
 
-    def __valid_provider(self, provider_name):
-        """
-        Test if a oauth/oauth2 provider is valid by asserting it has the client
-        key and secret set.
-        :param provider_name: (string) humanized name (e.g. Google Plus instead
-        of google-plus) of a oauth/oauth2 provider
-        :return: (boolean) True if valid, False otherwise
-        """
-        credentials = self.original_credentials.get(provider_name, None)
-        if credentials:
-            key = credentials.get('consumer_key', None)
-            secret = credentials.get('consumer_secret', None)
-            if key and secret:
-                return True
-        return False
+    """ Creates a  object with name & slug for oauth/oauth2 providers"""
+    
+    def __init__(self, name=None):
+        self.name = name
 
-    def get_name(self, provider_slug):
-        """
-        Returns the humanized name for a valid oauth/oauth2 provider.
-        :param provide_slug: (string) slug for a valid oauth/oauth2 provider
-        :return: (string|None) humanized name of a valid oauth/oauth2 provider
-        """
-        return str(self.providers.get(provider_slug, None))
-
-    def get_slugs(self):
-        return [str(slug) for slug in self.slugs]
+    @property
+    def slug(self):
+        return self.name.lower().replace(' ', '-')
